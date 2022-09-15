4f226d11a5c360a02390109b7d7b6f03f03ece05
JNicolao <bororio96@gmail.com>
2022-09-15 07:42:38 +0000

    Putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -16,7 +16,7 @@ int main(void)
 
 	for (ch = 0; ch < 8; ++ch)
 		_putchar(str[ch]);
-		_putchar('\n');
+	_putchar('\n');
 
 	return (0);
 }
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,5 +1,7 @@
-#ifndef _holberton_h_
-#define _holberton_h_
+#ifndef MAIN_H
+#define MAIN_H
+
+#include<stdio.h>
 
 int _putchar(char);
 void print_alphabet(void);
@@ -14,6 +16,5 @@ void times_table(void);
 int add(int, int);
 void print_to_98(int n);
 void print_times_table(int n);
-void num(int r, int n);
 
-#endif
+#endif /* MAIN_H */
