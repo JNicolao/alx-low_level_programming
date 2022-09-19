06dadc1db90122cf51087a0611fb1d2f778437f6
JNicolao <bororio96@gmail.com>
2022-09-19 10:01:10 +0000

    Reset

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - sets a pointer to a parameter then updates its value.
+ * @n: input integer.
+ * Return: nothing.
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+This directory contains projects on pointers, arrays and strings.
